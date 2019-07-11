4f9759248ca76c7bcd362954f1776cbc39bacd25
H4sh3 <pinkdaniel@web.de>
2019-07-11 17:07:15 +0200

    format code

--- environment/gym_colonizer/envs/colonizer_env.py
+++ environment/gym_colonizer/envs/colonizer_env.py
@@ -5,10 +5,10 @@ import torch
 
 from gym_colonizer.envs.field_types import CORN, WOOD, SHEEP, CLAY, IRON, DESERT
 from math import sqrt
-from gym import  error
+from gym import error
 import uuid
 from gym_colonizer.envs.colors import *
-from gym_colonizer.envs.field_types import WOOD, CLAY, CORN, DESERT, IRON,SHEEP
+from gym_colonizer.envs.field_types import WOOD, CLAY, CORN, DESERT, IRON, SHEEP
 
 
 class dotdict(dict):
@@ -40,8 +40,10 @@ def get_resources(shuffle=True):
         resources.append(IRON)
         resources.append(CLAY)
 
-    if shuffle: random.shuffle(resources)
+    if shuffle:
+        random.shuffle(resources)
     return resources
+
 
 class Spot():
     def __init__(self, x, y):
@@ -52,8 +54,8 @@ class Spot():
         self.close_resource = []
         self.close_road = []
         self.owner = 0
-    
-    def set_owner(self,owner):
+
+    def set_owner(self, owner):
         if self.owner is 0:
             self.owner = owner
             if owner == 1:
@@ -63,13 +65,12 @@ class Spot():
             return False
 
     def draw(self, pygame, screen):
-        print('draw')
-        #pygame.gfxdraw.filled_circle(screen, round(
-        #    self.pos.x), round(self.pos.y), self.radius, self.color)
+        pygame.gfxdraw.filled_circle(screen, round(
+            self.pos.x), round(self.pos.y), self.radius, self.color)
 
 
 class Resource():
-    def __init__(self, x, y,rating,resource):
+    def __init__(self, x, y, rating, resource):
         self.pos = dotdict({'x': x, 'y': y})
         self.radius = 25
         self.id = uuid.uuid1()
@@ -77,26 +78,28 @@ class Resource():
         self.rating = rating
         self.resource = resource
         self.color = get_color(self.resource)
+        print(self.color)
 
     def draw(self, pygame, screen):
         pygame.gfxdraw.filled_circle(screen, round(
             self.pos.x), round(self.pos.y), self.radius, self.color)
-    
+
         font = pygame.font.SysFont("arial", 25)
-        text = font.render(str(self.number), 3, (0, 0, 255))
+        text = font.render(str(self.rating), 3, (0, 0, 255))
         screen.blit(text, (self.pos.x-10, self.pos.y-10))
 
 
 class Road():
-    def __init__(self, x1, y1 ,x2,y2,s1_id,s2_id):
-        self.pos = dotdict({'x1': int(x1), 'y1': int(y1),'x2': int(x2), 'y2': int(y2)})
+    def __init__(self, x1, y1, x2, y2, s1_id, s2_id):
+        self.pos = dotdict({'x1': int(x1), 'y1': int(y1),
+                            'x2': int(x2), 'y2': int(y2)})
         self.radius = 3
         self.color = (100, 200, 100)
         self.id = uuid.uuid1()
-        self.close_spot = [s1_id,s2_id]
+        self.close_spot = [s1_id, s2_id]
 
     def draw(self, pygame, screen):
-        pygame.gfxdraw.line(screen, *self.pos.values() , self.color)
+        pygame.gfxdraw.line(screen, *self.pos.values(), self.color)
 
 
 class ColonizerEnv(gym.Env):
@@ -116,7 +119,7 @@ class ColonizerEnv(gym.Env):
     def get_state(self):
         state = []
         for r in self.resources:
-            state.append(self.resources[r].number)
+            state.append(self.resources[r].rating)
 
         for s in self.spots:
             state.append(self.spots[s].owner)
@@ -153,55 +156,57 @@ class ColonizerEnv(gym.Env):
 
     def add_roads_and_resources(self):
         used_positions = []
-        
-        rating = [2,3,3,4,4,5,5,6,6,7,8,8,9,9,10,10,11,11,12]
+
+        rating = [2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12]
         resources = get_resources()
-        
+
         random.shuffle(rating)
-        print('312 ',len(self.spots))
+        print('312 ', len(self.spots))
         for key_s1 in self.spots:
             s1 = self.spots[key_s1]
             for key_s2 in self.spots:
                 s2 = self.spots[key_s2]
-                if s1 == s2: continue
+                if s1 == s2:
+                    continue
                 else:
                     dist = round(sqrt((s2.pos.x - s1.pos.x) **
                                       2 + (s2.pos.y - s1.pos.y)**2))
                     x = (s1.pos.x+s2.pos.x)/2
                     y = (s1.pos.y+s2.pos.y)/2
-                    if [x,y] in used_positions: continue
+                    if [x, y] in used_positions:
+                        continue
 
                     # Roads
                     if dist == 53 or dist == 35:
-                        road = Road(s1.pos.x, s1.pos.y, s2.pos.x,s2.pos.y,s1.id,s2.id)
+                        road = Road(s1.pos.x, s1.pos.y, s2.pos.x,
+                                    s2.pos.y, s1.id, s2.id)
                         self.roads[road.id] = road
-                        used_positions.append([x,y])
+                        used_positions.append([x, y])
 
                     # Resources
                     if dist == 105:
                         print(len(rating))
-                        #n = rating.pop()
-                        n = 1
-                        if n == 7:
-                            resource = Resource(x, y, n, DESERT)
+                        r = rating.pop()
+                        if r == 7:
+                            resource = Resource(x, y, r, DESERT)
                         else:
-                            #resource = Resource(x,y,n,rating.pop())
-                            resource = Resource(x,y,n,6)
-                        self.resources[resource.id]=resource
-                        used_positions.append([x,y])
-    
+                            resource = Resource(x, y, r, resources.pop())
+                        self.resources[resource.id] = resource
+                        used_positions.append([x, y])
+
     def add_relations(self):
         # relation between spots and resources
         for s in self.spots:
             spot = self.spots[s]
             for r in self.resources:
                 res = self.resources[r]
-                dist = round(sqrt((spot.pos.x - res.pos.x) ** 2 + (spot.pos.y - res.pos.y)**2))
+                dist = round(sqrt((spot.pos.x - res.pos.x) **
+                                  2 + (spot.pos.y - res.pos.y)**2))
                 if dist == 52 or dist == 44:
                     self.spots[s].close_resource.append(res.id)
                     self.resources[r].close_spot.append(spot.id)
-                    self.lines.append([int(spot.pos.x),int(spot.pos.y),int(res.pos.x),int(res.pos.y)])
-
+                    self.lines.append([int(spot.pos.x), int(
+                        spot.pos.y), int(res.pos.x), int(res.pos.y)])
 
     def reset(self):
         self.screen = None
@@ -235,7 +240,7 @@ class ColonizerEnv(gym.Env):
 
                 self.screen.fill((255, 255, 255))
                 for line in self.lines:
-                    pygame.gfxdraw.line(self.screen, *line,BLACK)
+                    pygame.gfxdraw.line(self.screen, *line, BLACK)
                 for key in self.resources:
                     self.resources[key].draw(pygame, self.screen)
 
@@ -250,23 +255,23 @@ class ColonizerEnv(gym.Env):
         else:
             raise error.UnsupportedMode("Unsupported render mode: " + mode)
 
-    def step(self,action):
+    def step(self, action):
         cnt = 0
         for key in self.spots.keys():
             if cnt == action:
                 if self.spots[key].set_owner(1):
                     # worked
                     #print('legal move')
-                    return 10 # return reward based on surrounding resources rating
+                    return 10  # return reward based on surrounding resources rating
                 else:
                     #print('illegal move')
                     # illegal action
                     return -1
-            cnt+=1
+            cnt += 1
 #
         #d1 = random.randint(1,6)
         #d2 = random.randint(1,6)
 #
         #n = d1+d2
-        #print('dice',n)
+        # print('dice',n)
 #
